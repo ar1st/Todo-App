b7191945b008cd2ed422f521343d0315130b8e87
ar1st <aristnm@hotmail.com>
2020-10-24 14:29:55 -0700

    connecting to api

--- src/components/WelcomeComponent.jsx
+++ src/components/WelcomeComponent.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import {  Link } from 'react-router-dom'
+import HelloWorldService from '../api/todo/HelloWorldService.js'
 
 class WelcomeComponent extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            welcomeMessage : ''
+        }
+    }
     render() {
         
         return (
@@ -11,10 +18,27 @@ class WelcomeComponent extends Component {
                     Welcome {this.props.match.params.name}.
                     Manage your todos <Link to="/todos">here</Link>.
                 </div>
+                <div className="container">
+                    Click here to get a customized welcome
+                    <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Click to get message</button>
+                </div>
+                <div className="container">
+                    {this.state.welcomeMessage}
+                </div>
             </>
 
 
         )
+    }
+
+    retrieveWelcomeMessage = () =>{
+        HelloWorldService.executeHelloWorldService()
+        .then(response => this.handleSuccessfulResponse(response))
+        //.catch()
+    }
+
+    handleSuccessfulResponse = (response) => {
+        this.setState ({ welcomeMessage:response.data})
     }
 }
 
